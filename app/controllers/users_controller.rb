afa41a60e491295f6fd0289fef5b9aed62c75ab8
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-30 14:53:18 -0400

    Create user and video game controller, update application controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation, :bio)
+  end
+end
