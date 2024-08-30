527719e530a46c79b8d014ab3e588e9259a17c87
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-30 14:52:11 -0400

    Update user model or bycrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :collections
   has_many :reviews
   has_many :video_games, through: :collections
+
+  validates :username, presence: true, uniqueness: true
+  validates :bio, presence: true
 end
