14f105262727c1a88fbb4981fcc582b7019ce3db
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-30 14:51:23 -0400

    Update users to work with bycrypt

--- db/migrate/20240830144623_create_users.rb
+++ db/migrate/20240830144623_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password
+      t.string :username, null: false
+      t.string :password_digest, null: false
       t.text :bio
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -48,8 +48,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_30_151327) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
+    t.string "username", null: false
+    t.string "password_digest", null: false
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
