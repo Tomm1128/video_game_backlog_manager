b55b8690020bf9dcb67071ae0697455fc243c01a
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-09-03 09:47:45 -0400

    Update tests to use factory bot

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    username { "Tuj" }
+    password { "password" }
+    bio { "This is a bio" }
+  end
+end
--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature "User login", type: :feature do
-  let!(:user) { User.create!(username: 'testuser', password: 'password123', password_confirmation: 'password123', bio: 'A gamer.') }
+  # let!(:user) { User.create!(username: 'testuser', password: 'password123', password_confirmation: 'password123', bio: 'A gamer.') }
 
   scenario 'with valid credentials' do
+    user = create(:user)
+
     visit login_path
 
     fill_in 'Username', with: user.username
-    fill_in 'Password', with: 'password123'
+    fill_in 'Password', with: 'password'
     click_button 'Log in'
 
     expect(page).to have_current_path(root_path)
   end
 
   scenario 'with invalid credentials' do
+    user = create(:user)
+
     visit login_path
 
     fill_in 'Username', with: user.username
