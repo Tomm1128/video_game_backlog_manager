f940e0248f2265d8d3ab2cfa1380097b4edf106f
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-09-04 14:51:41 -0400

    Add user to show action

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,9 +1,12 @@
 class VideoGamesController < ApplicationController
+  def welcome; end
+
   def index
     @video_games = VideoGame.all
   end
 
   def show
     @video_game = VideoGame.find(params[:id])
+    @user = User.find(session[:user_id])
   end
 end
