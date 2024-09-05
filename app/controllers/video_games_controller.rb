f1823f5612056cea578011ad32e694eece1605c3
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-09-05 10:24:24 -0400

    Update welcome action with data for homepage

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -1,10 +1,12 @@
 class VideoGamesController < ApplicationController
   before_action :authenticate_user
 
-  def welcome; end
+  def welcome
+    @top_rated_video_games = VideoGame.all.sort_by(&:average_rating).reverse.first(3)
+  end
 
   def index
-    @video_games = VideoGame.all
+    @video_games = VideoGame.all.sort_by(&:average_rating).reverse
   end
 
   def show
